38a0c92a929ac6874a4545018bc0979fec9e433c
Stefano Teso <stefano.teso@gmail.com>
2015-09-07 13:36:10 +0200

    main: implement the housing dataset.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 import os
 import numpy as np
-from scipy.io import loadmat
 from sklearn.utils import check_random_state
 import itertools as it
 import solver
@@ -12,7 +11,7 @@ import solver
 # TODO: hybrid case
 
 def onehot(domain_size, value):
-    assert 0 <= value < domain_size
+    assert 0 <= value < domain_size, "out-of-bounds value: 0/{}/{}".format(value, domain_size)
     value_onehot = np.zeros(domain_size, dtype=np.int8)
     value_onehot[value] = 1
     return value_onehot
@@ -40,7 +39,53 @@ def get_pc_dataset():
     raise NotImplementedError
 
 def get_housing_dataset():
-    raise NotImplementedError
+    from scipy.io import loadmat
+
+    # See AISTATS_Housing_Uniform/readXML.m
+    CATEGORICAL_FEATURES = [i-1 for i in [4, 9]]
+    SCALAR_FEATURES = [i-1 for i in [1, 2, 3, 5, 6, 7, 8, 10, 11, 12, 13, 14]]
+    INTERVAL = 10.0
+
+    items = loadmat("houseMatlab.mat")["housing"]
+    assert items.shape == (506, 14)
+
+    num_items, num_features = items.shape
+
+    attribute_info = [None for _ in range(num_features)]
+    for z in CATEGORICAL_FEATURES:
+        attribute_info[z] = sorted(set(items[:,z]))
+    for z in SCALAR_FEATURES:
+        l = min(items[:,z])
+        u = max(items[:,z])
+        # XXX the 1e-6 is there because MATLAB's arange notation is inclusive
+        # wrt the upper bound
+        attribute_info[z] = np.arange(l-(u-l)/INTERVAL, u+(u-l)/INTERVAL+1e-6, (u-l)/i)
+
+    discretized_items = np.zeros(items.shape, dtype=np.int32)
+    for i, item in enumerate(items):
+        for z, value in enumerate(item):
+            if z in CATEGORICAL_FEATURES:
+                discretized_items[i,z] = attribute_info[z].index(value)
+            else:
+                temp = value - attribute_info[z]
+                discretized_items[i,z] = np.where(temp <= 0)[0][0]
+
+    domain_sizes = []
+    for z in range(num_features):
+        domain_sizes.append(max(discretized_items[:,z]) - min(discretized_items[:,z]) + 1)
+        print max(discretized_items[:,z]), min(discretized_items[:,z]), "-->", domain_sizes[z]
+
+    items_onehot = None
+    for item in discretized_items:
+        item_onehot = np.hstack((onehot(domain_sizes[z], attribute_value - min(discretized_items[:,z]))
+                                 for z, attribute_value in enumerate(item)))
+        if items_onehot is None:
+            items_onehot = item_onehot
+        else:
+            items_onehot = np.vstack((items_onehot, item_onehot))
+    assert items_onehot.shape == (506, sum(domain_sizes))
+
+    return domain_sizes, items_onehot, np.array([]), np.array([])
 
 def sample_utility(domain_sizes, rng, mode="uniform"):
     """Samples a utility weight vector.
