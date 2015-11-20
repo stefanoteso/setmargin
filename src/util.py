2323c2f1d617bbd9be78062514a071d9a339f617
Stefano Teso <stefano.teso@gmail.com>
2015-11-20 11:38:05 +0100

    util: move prod() here.

--- src/util.py
+++ src/util.py
@@ -7,6 +7,9 @@ import numpy as np
 def _cls(obj):
     return type(obj).__name__
 
+def prod(l):
+    return l[0] if len(l) == 1 else l[0]*prod(l[1:])
+
 def onehot(domain_size, value):
     assert 0 <= value < domain_size, "out-of-bounds value: 0/{}/{}".format(value, domain_size)
     value_onehot = np.zeros(domain_size, dtype=np.int8)
