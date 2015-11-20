4760b87ee928809f59a90c01a5e9ed8f20662df0
Stefano Teso <stefano.teso@gmail.com>
2015-11-20 16:31:15 +0100

    run.sh: shrink the number of runs.

--- src/run.sh
+++ src/run.sh
@@ -2,17 +2,13 @@
 
 for dataset in synthetic constsynthetic; do
     for set_size in 1 2 3 4; do
-        for multimargin in "" "-M"; do
-            for usermode in "" "--is-deterministic" "--is-indifferent"; do
-                for samplingmode in uniform normal; do
-                    command="./main.py synthetic -N 10 -m $set_size -a 10 $multimargin $usermode -u $samplingmode -s 0 --debug"
-                    $command
-                    if [ $? -ne 0 ]; then
-                        echo "$command failed."
-                        exit 1
-                    fi
-                done
-            done
+        for samplingmode in uniform normal; do
+            command="./main.py synthetic -N 10 -m $set_size -a 10 --is-indifferent -u $samplingmode -s 0 --debug"
+            $command
+            if [ $? -ne 0 ]; then
+                echo "$command failed."
+                exit 1
+            fi
         done
     done
 done
