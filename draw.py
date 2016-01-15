8a49644c9ba88e98c8ba5c41d66de89d3304c9b9
Stefano Teso <stefano.teso@gmail.com>
2016-01-15 11:06:51 +0100

    update the drawing script.

--- draw.py
+++ draw.py
@@ -7,6 +7,10 @@ from matplotlib.font_manager import FontProperties
 import itertools as it
 from glob import glob
 
+if len(sys.argv) != 3:
+    print "Usage: {} <results directory> <output image>".format(sys.argv[0])
+    sys.exit(1)
+
 fontP = FontProperties()
 fontP.set_size('small')
 
@@ -23,11 +27,11 @@ for path in paths:
     num_trials, max_queries = loss_matrix.shape
 
     loss_means = np.mean(loss_matrix, axis=0)
+    loss_stds = np.std(loss_matrix, axis=0).reshape(-1, 1)
 
-    ax.set_ylim([0.0, max(0.5, max(loss_means) + 0.1)])
-    ax.errorbar(np.arange(1, max_queries + 1), loss_means, label=os.path.basename(path))
+    ax.set_ylim([0.0, max(0.5, max(loss_means) + max(loss_stds) + 0.1)])
+    ax.errorbar(np.arange(1, max_queries + 1), loss_means, yerr=loss_stds, fmt="--o", label=os.path.basename(path))
 
-lgd = ax.legend(prop=fontP, loc="upper center", bbox_to_anchor=(0.5, -0.1))
-
-path = os.path.join(sys.argv[1], "plot.svg")
-fig.savefig(path, bbox_extra_artists=(lgd,), bbox_inches="tight")
+#lgd = ax.legend(prop=fontP, loc="upper center", bbox_to_anchor=(0.5, -0.1))
+#fig.savefig(sys.argv[2], bbox_extra_artists=(lgd,), bbox_inches="tight")
+fig.savefig(sys.argv[2], bbox_inches="tight")
---
+++ draw_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for domain_size in `seq 2 5`; do
+    for sampling_mode in uniform uniform_sparse normal normal_sparse; do
+        echo "drawing $domain_size $sampling_mode"
+        ./draw.py results/synthetic_${domain_size}/${sampling_mode} paper/figure_synthetic_${domain_size}_${sampling_mode}.png
+    done
+done
