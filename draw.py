6300257232960130777c74517fc34008d6c058f8
Stefano Teso <stefano.teso@gmail.com>
2016-01-16 09:52:12 +0100

    update drawing scripts.

--- draw.py
+++ draw.py
@@ -20,18 +20,47 @@ ax.set_ylabel("Average loss over trials")
 
 paths = sorted(glob(os.path.join(sys.argv[1], "results_*_loss_matrix.txt")))
 
+# Colors taken from http://tango.freedesktop.org/Tango_Icon_Theme_Guidelines
+COLOR_OF = {
+#    ("#EDD400", "#FCE94F"), # yellow
+    2: ("#CE5C00", "#F57900"), # orange
+#    ("#4E9A06", "#73D216"), # green
+    3: ("#204A87", "#3465A4"), # blue
+#    ("#5C3566", "#75507B"), # violet
+#    2: ("#A40000", "#CC0000"), # red
+}
+
+max_max_x = 0
+max_max_y = 0
 for path in paths:
     parts = os.path.basename(path).split("__")
+    set_size = int(parts[1].split("=")[1])
+    if not set_size in (2, 3):
+        continue
 
     loss_matrix = np.loadtxt(path)
+    if loss_matrix.shape[1] > 100:
+        loss_matrix = loss_matrix[:,:100]
     num_trials, max_queries = loss_matrix.shape
 
-    loss_means = np.mean(loss_matrix, axis=0)
-    loss_stds = np.std(loss_matrix, axis=0).reshape(-1, 1)
+    xs = np.arange(max_queries)
+    ys = np.median(loss_matrix, axis=0)
+    yerrs = np.std(loss_matrix, axis=0)
 
-    ax.set_ylim([0.0, max(0.5, max(loss_means) + max(loss_stds) + 0.1)])
-    ax.errorbar(np.arange(1, max_queries + 1), loss_means, yerr=loss_stds, fmt="--o", label=os.path.basename(path))
+    if max_queries > max_max_x:
+        max_max_x = max_queries
+    max_y = max(ys + yerrs)
+    if max_y > max_max_y:
+        max_max_y = max_y
 
-#lgd = ax.legend(prop=fontP, loc="upper center", bbox_to_anchor=(0.5, -0.1))
-#fig.savefig(sys.argv[2], bbox_extra_artists=(lgd,), bbox_inches="tight")
+    fg, bg = COLOR_OF[set_size]
+
+    ax.plot(xs, ys, "k-", linewidth=2.0, color=fg)
+    ax.fill_between(xs, ys - yerrs, ys + yerrs, color=bg, alpha=0.35, linewidth=0)
+
+
+max_max_y = max(0.5, max_max_y + 0.1)
+ax.set_xticks(np.arange(0, max_max_x, 10))
+ax.set_yticks(np.arange(0, max(0.5, max_max_y + 0.1), 0.1))
+ax.set_ylim([0.0, max_max_y])
 fig.savefig(sys.argv[2], bbox_inches="tight")
--- draw_all.sh
+++ draw_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for domain_size in `seq 2 5`; do
+for domain_size in `seq 3 6`; do
     for sampling_mode in uniform uniform_sparse normal normal_sparse; do
         echo "drawing $domain_size $sampling_mode"
         ./draw.py results/synthetic_${domain_size}/${sampling_mode} paper/figures/synthetic_${domain_size}_${sampling_mode}.png
